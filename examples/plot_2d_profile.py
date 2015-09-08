1f28e79f78f8c7de92c86a23c29f9e41950288b2
goetz <theodore.goetz@gmail.com>
2015-09-08 16:18:27 -0400

    minor edit.

--- examples/plot_2d_profile.py
+++ examples/plot_2d_profile.py
@@ -15,20 +15,21 @@ h2 = Histogram(d0,d1,'$z$','Random Data')
 h2.fill(xdata,ydata)
 
 fig,ax = pyplot.subplots()
+
 hprof,fitslices,fitprof = ax.plothist_profile(h2, cmap=cm.Blues)
 
 popt,pcov,ptest = fitprof
 perr = np.sqrt(popt)
 
-kw = dict(
+msg = '''\
+$N = {opt[0]:.0f} \pm {err[0]:.0f}$
+$\mu = {opt[1]:.1f} \pm {err[1]:.1f}$'''
+
+ax.text(.05, .95,
+    msg.format(opt=popt,err=perr),
     horizontalalignment = 'left',
     verticalalignment = 'top',
     bbox = dict(alpha = 0.7, color='white'),
     transform = ax.transAxes)
-
-msg = '''\
-$N = {opt[0]:.0f} \pm {err[0]:.0f}$
-$\mu = {opt[1]:.1f} \pm {err[1]:.1f}$'''
-ax.text(.05, .95, msg.format(opt=popt,err=perr), **kw)
 
 pyplot.show()
