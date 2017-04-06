7d0d8503dca647ad85aa9e71026a32afa9d5ccc8
John Theodore Goetz <theodore.goetz@gmail.com>
2017-04-05 20:04:35 -0700

    more image comparison tests

--- test/graphics/test_histogram_mpl.py
+++ test/graphics/test_histogram_mpl.py
@@ -17,22 +17,45 @@ from histogram import Histogram
 
 from .. import comparator
 
-class TestImage(unittest.TestCase):
+class TestHistogramsMpl(unittest.TestCase):
     def setUp(self):
         simplefilter('ignore')
 
     def test_hist_1d(self):
-
         rand.seed(1)
-
         h = Histogram(40, [0,1], 'χ (cm)', 'counts', 'Some Data')
         h.fill(rand.normal(0.5, 0.1, 2000))
-
         fig, ax = plt.subplots()
         pt = ax.plothist(h)
-
         self.assertTrue(comparator.compare_images(
             fig.savefig, 'hist_1d.png'))
+
+    def test_hist_1d_errorbar(self):
+        rand.seed(1)
+        h = Histogram(40, [0,1], 'χ (cm)', 'counts', 'Some Data')
+        h.fill(rand.normal(0.5, 0.1, 300))
+        fig, ax = plt.subplots()
+        pt = ax.plothist(h, style='errorbar')
+        self.assertTrue(comparator.compare_images(
+            fig.savefig, 'hist_1d_errorbar.png'))
+
+    def test_hist_1d_line(self):
+        rand.seed(1)
+        h = Histogram(40, [0,1], 'χ (cm)', 'counts', 'Some Data')
+        h.fill(rand.normal(0.5, 0.1, 300))
+        fig, ax = plt.subplots()
+        pt = ax.plothist(h, style='line')
+        self.assertTrue(comparator.compare_images(
+            fig.savefig, 'hist_1d_line.png'))
+
+    def test_hist_2d(self):
+        rand.seed(1)
+        h = Histogram(40, [0,1], 'χ (cm)', 30, [-5,5], 'ψ', 'counts', 'Some Data')
+        h.fill(rand.normal(0.5, 0.1, 1000), rand.normal(0,1,1000))
+        fig, ax = plt.subplots()
+        pt = ax.plothist(h)
+        self.assertTrue(comparator.compare_images(
+            fig.savefig, 'hist_2d.png'))
 
 
 if __name__ == '__main__':
